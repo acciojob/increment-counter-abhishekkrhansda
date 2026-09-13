9055c03b087f92e0be0127862499cca3ad9c5438
abhishek kumar hansda <118197040+abhishekkrhansda@users.noreply.github.com>
2026-09-13 16:27:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let para = document.querySelector("#counter");
+let btn = document.querySelector("#incrementBtn");
+
+btn.addEventListener("click",() =>{
+	let number = Number(para.innerText) + 1;
+	para.innerText = number;
+})
